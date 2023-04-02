77551f8bfaa17d7fa50eb2ee882511670b20ffa7
Your Name <chetandasa2814@gmail.com>
2023-04-02 23:25:33 +0530

    OpenAI Codex

--- AI_first_application/client/script.js
+++ AI_first_application/client/script.js
@@ -1,15 +1,15 @@
 import bot from './assets/bot.svg';
 import user from './assets/user.svg';
 
-const from = document.querySelector('form');
-const chatContainer = document.querySelector('#chat_Container');
+const form = document.querySelector('form');
+const chatContainer = document.querySelector('#chat_container');
 
 let loadInterval;
 
 function loader(element) {
   element.textContent = '';
 
-  loadInterval = serInterval(() => {
+  loadInterval = setInterval(() => {
     element.textContent += '.';
 
     if (element.textContent === '....') {
@@ -22,8 +22,8 @@ function typeText(element, text) {
   let index = 0;
 
   let interval = setInterval(() => {
-    if (index < text.length) {
-      element.innerHTML += text.chatAT(index);
+    if(index < text.length) {
+      element.innerHTML += text.charAt(index);
       index++;
     } else {
       clearInterval(interval);
@@ -32,13 +32,83 @@ function typeText(element, text) {
 }
 
 function generateUniqueId() {
-  const timestamp = Data.now();
+  const timestamp = Date.now();
   const randomNumber = Math.random();
   const hexadecimalString = randomNumber.toString(16);
 
-  return `id-${timestamp}-$(hexadecimalString)`;
+  return `id-${timestamp}-${hexadecimalString}`;
 }
 
 function chatStripe (isAi, value, uniqueId) {
-  
+  return (
+    `
+      <div class="wrapper ${isAi && 'ai'}">
+        <div class="chat">
+          <div class="profile">
+            <img 
+              src="${isAi ? bot : user}"
+              alt="${isAi ? 'bot' : 'user'}"
+            />
+          </div>
+          <div class="message" id=${uniqueId}>${value}></div>
+        </div>
+      </div>
+    `
+  )
 }
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  const data = new FormData(form);
+
+  //user's chatstripe
+  chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
+
+  form.reset();
+
+  //bot's chatstrips
+  const uniqueId = generateUniqueId();
+  chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
+
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+
+  const messageDiv = document.getElementById(uniqueId);
+
+  loader(messageDiv);
+
+  //fetch data from server -> bot's responce
+
+  const response = await fetch('http://localhost/5000', {
+    method: 'POST',
+    headers:{
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      prompt: data.get('prompt')
+    })
+  })
+
+   clearInterval(loadInterval);
+   messageDiv.innerHTML = '';
+
+   if(response.ok) {
+    const data = await response.json();
+    const parsedData = data.bot.trim();
+
+    typeText(messageDiv, parsedData);
+   } else {
+    const err = await response.text();
+
+    messageDiv.innerHTML = "Something went wrong";
+
+    alert(err);
+   }
+}
+
+form.addEventListener('submit', handleSubmit);
+form.addEventListener('keyup', (e) => {
+  if (e.keyCode === 13) {
+    handleSubmit(e);
+  }
+})

936c84a8d3dc9c5fab7abee8074a46ff73c07811
Your Name <chetandasa2814@gmail.com>
2023-04-29 13:55:00 +0530

    OPEN AI Summary maker

---
+++ AI-Website/src/services/article.js
@@ -0,0 +1,43 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
+
+export const articleApi = createApi({
+    reducerPath: 'articleApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com/',
+        prepareHeaders: (headers) => {
+            headers.set('X-RapidAPI-Key', rapidApiKey);
+            headers.set('X-RapidAPI-Host', 'article-extractor-and-summarizer.p.rapidapi.com');
+
+            return headers;
+        },
+    }),
+    endpoints: (builder) => ({
+        getSummary: builder.query({
+            // encodeURIComponent() function encodes special characters that may be present in the parameter values
+            // If we do not properly encode these characters, they can be misinterpreted by the server and cause errors or unexpected behavior. Thus that RTK bug
+            query: (params) => `/summarize?url=${encodeURIComponent(params.articleUrl)}&length=3`,
+        }),
+    }),
+})
+
+export const { useLazyGetSummaryQuery } = articleApi
+
+
+// THIS IS TAKEN BY RAPID API WEBSITE LINK IS DOWN
+// https://rapidapi.com/restyler/api/article-extractor-and-summarizer
+
+// const options = {
+//     method: 'GET',
+//     url: 'https://article-extractor-and-summarizer.p.rapidapi.com/summarize',
+//     params: {
+//       url: 'https://time.com/6266679/musk-ai-open-letter/',
+//       length: '3'
+//     },
+//     headers: {
+//       'content-type': 'application/octet-stream',
+//       'X-RapidAPI-Key': 'dbb8417968msh51585addd166542p1ce0a1jsn171a2967823b',
+//       'X-RapidAPI-Host': 'article-extractor-and-summarizer.p.rapidapi.com'
+//     }
+//   };
